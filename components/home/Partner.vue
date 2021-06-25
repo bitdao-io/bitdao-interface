d9b88dfd47dc3b048d3920c368b3facff134ee9c
zxplus <shellteo@163.com>
2021-06-25 14:48:56 +0800

    feat: update partners

--- components/home/Partner.vue
+++ components/home/Partner.vue
@@ -1,85 +1,33 @@
 <template>
   <section class="partner">
     <h1>Partners</h1>
-    <table class="partner-table" cellpadding="0" cellspacing="0" border="0">
-      <colgroup>
-        <col width="16.67%">
-        <col width="16.67%">
-        <col width="16.67%">
-        <col width="16.67%">
-        <col width="16.67%">
-        <col width="16.67%">
-      </colgroup>
-      <tbody class="partner-tbody">
-        <tr>
-          <td><img src="@/assets/image/partner/peter.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/founders.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/pantera.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/dragonfly.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/bybit.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/alan.svg" alt=""></td>
-        </tr>
-        <tr>
-          <td><img src="@/assets/image/partner/kain.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/fenbushi.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/spartan.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/jump.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/cambium.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/sushi.svg" alt=""></td>
-        </tr>
-        <tr>
-          <td><img src="@/assets/image/partner/tembusu.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/longhash.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/iangels.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/alphanonce.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/coinhako.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/mgnr.svg" alt=""></td>
-        </tr>
-        <tr>
-          <td><img src="@/assets/image/partner/ngc.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/uva.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/la.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/amber.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/peak.svg" alt=""></td>
-          <td><img src="@/assets/image/partner/preangel.svg" alt=""></td>
-        </tr>
-      </tbody>
-    </table>
-    <!-- <div class="partner-row">
+    <div class="partner-logos">
       <img src="@/assets/image/partner/peter.svg" alt="">
       <img src="@/assets/image/partner/founders.svg" alt="">
       <img src="@/assets/image/partner/pantera.svg" alt="">
       <img src="@/assets/image/partner/dragonfly.svg" alt="">
       <img src="@/assets/image/partner/bybit.svg" alt="">
       <img src="@/assets/image/partner/alan.svg" alt="">
-    </div>
-
-    <div class="partner-row">
       <img src="@/assets/image/partner/kain.svg" alt="">
       <img src="@/assets/image/partner/fenbushi.svg" alt="">
       <img src="@/assets/image/partner/spartan.svg" alt="">
       <img src="@/assets/image/partner/jump.svg" alt="">
+      <img src="@/assets/image/partner/peak.svg" alt="">
       <img src="@/assets/image/partner/cambium.svg" alt="">
-      <img src="@/assets/image/partner/sushi.svg" alt="">
-    </div>
-
-    <div class="partner-row">
       <img src="@/assets/image/partner/tembusu.svg" alt="">
       <img src="@/assets/image/partner/longhash.svg" alt="">
       <img src="@/assets/image/partner/iangels.svg" alt="">
       <img src="@/assets/image/partner/alphanonce.svg" alt="">
       <img src="@/assets/image/partner/coinhako.svg" alt="">
       <img src="@/assets/image/partner/mgnr.svg" alt="">
-    </div>
-
-    <div class="partner-row">
       <img src="@/assets/image/partner/ngc.svg" alt="">
       <img src="@/assets/image/partner/uva.svg" alt="">
       <img src="@/assets/image/partner/la.svg" alt="">
       <img src="@/assets/image/partner/amber.svg" alt="">
-      <img src="@/assets/image/partner/peak.svg" alt="">
+      <img src="@/assets/image/partner/sushi.svg" alt="">
       <img src="@/assets/image/partner/preangel.svg" alt="">
-    </div> -->
+      <img src="@/assets/image/partner/fin.svg" alt="">
+    </div>
   </section>
 </template>
 
@@ -95,6 +43,16 @@
     font-weight: 700;
     color: #0E47EF;
     margin-bottom: 45px;
+  }
+  .partner-logos {
+    width: 1000px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    img {
+      height: 80px;
+    }
   }
   table {
     width: 600px;
