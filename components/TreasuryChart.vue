5b137282841e4e3bea9326685ebae36b73e9e350
zxplus <shellteo@163.com>
2021-07-09 17:54:06 +0800

    fix: typo

--- components/TreasuryChart.vue
+++ components/TreasuryChart.vue
@@ -35,7 +35,7 @@
         <p class="subtitle">
         </p>
         <PledgedBalance />
-        <p class="tips" v-if="$t('PartnerContributionsTips')"></p>
+        <p class="tips" v-html="$t('PartnerContributionsTips')"></p>
         <div class="box-button-container">
           <a
             href="/analytics"
